575b4cfc97c56a5f53c17fa63d68bca21b33a177
blueswayne133 <blueswayne133@gmail.com>
2025-05-01 03:25:27 -0700

    new changes

--- resources/views/home/past_exhibition.blade.php
+++ resources/views/home/past_exhibition.blade.php
@@ -1,8 +1,5 @@
 @include('home.header')
 <main id="content">
-
-    <span id="pp_page_instance_746"></span>
-
     <div class="container-fluid align-" style="padding-top: 30px; padding-bottom: 30px; background: #fff;">
         <div class="container">
             <div class="infoHeaderBoxedImage" data-animations="">
@@ -19,54 +16,34 @@
         </div>
     </div>
 
-    <div class="container-fluid infoHeaderWrapper type-page align-"
-        style="padding-top: 30px; padding-bottom: 30px; background: #fff;">
-        <div class="container">
-            <div class="infoHeaderContent">
-                <div class="inner">
-                    <h1>Past Exhibitions</h1>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-fluid desc2Wrapper variant- align-"
-        style="padding-top: 30px; padding-bottom: 30px; background: #CCD5D8;">
-        <div class="container">
-            <div class="richtext">
-                <p>Explore our archive of past exhibitions that have graced our halls. These shows represent the diverse
-                    range of artistic experiences we've presented over the years.</p>
-                <p>Many of our past exhibitions have accompanying catalogues and merchandise available in our.</p>
-            </div>
-        </div>
-    </div>
-
     <div class="container-fluid listWrapper theme" style="padding-top: 0px; padding-bottom: 50px; background: #fff;">
         <div class="container">
             <ul data-animations="zoom" class="listItems variant-">
                 @foreach($pastExhibitions as $exhibition)
                 <li data-entry-id="{{ $exhibition->id }}"
                     class="eventCard context-default production-type-default variant-default topdate">
+                    <style>
+                        [data-entry-id="{{ $exhibition->id }}"] .thumb .image {
+                            background-image: url('{{ $exhibition->picture_url ?? ' https: //via.placeholder.com/855x600' }}');
+                            }
+                    </style>
                     <div class="listItemWrapper">
                         <div class="thumb">
-                            @if($exhibition->video_url)
-                            <video class="video-thumbnail" controls
-                                style="width: 100%; height: 100%; object-fit: cover;">
+                            <a class="image" href="/exhibitions/{{ $exhibition->id }}" tabindex="-1"></a>
+                        </div>
+                        @if($exhibition->video_url)
+                        <div class="video-container">
+                            <video controls>
                                 <source src="{{ $exhibition->video_url }}" type="video/mp4">
                                 Your browser does not support the video tag.
                             </video>
-                            @else
-                            <div class="image"
-                                style="background-image: url('{{ $exhibition->picture_url ?? 'https://via.placeholder.com/855x600' }}'); height: 300px; background-size: cover;">
-                            </div>
-                            @endif
                         </div>
+                        @endif
                     </div>
                 </li>
                 @endforeach
             </ul>
         </div>
     </div>
-
 </main>
 @include('home.footer')
